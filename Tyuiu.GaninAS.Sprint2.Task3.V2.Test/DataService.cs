65d51c7643da90bd98e108c6704d38cc28a8ef24
AndreyGanin <ganin25@inbox.ru>
2025-10-21 14:57:19 +0500

    Task3

---
+++ Tyuiu.GaninAS.Sprint2.Task3.V2.Test/DataService.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.GaninAS.Sprint2.Task3.V2.Lib;
+
+namespace Tyuiu.GaninAS.Sprint2.Task3.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double res = ds.Calculate(x);
+            double wait = -44.654;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GaninAS.Sprint2.Task3.V2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.GaninAS.Sprint2.Task3.V2.Lib;
+namespace Tyuiu.GaninAS.Sprint2.Task3.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Спринт #2 | Выполнил: Ганин Андрей Сергеевич | ПКТБ-25-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #2                                                             *");
+            Console.WriteLine("* Выполнил: Ганин Андрей Сергеевич | ПКТБ-25-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*Написать программу, которая вычисляет требуемое значение функции Y с испо*");
+            Console.WriteLine("*ользованием вложенных оператор if-else, где пользователь вводит значение *");
+            Console.WriteLine("* переменной X с клавиатуры. Округлить полученное значение до трех знаков *");
+            Console.WriteLine("* после запятой;                                                          *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            Console.WriteLine("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double result = ds.Calculate(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Значение функции = " + result);
+            Console.ReadKey();
+        }
+    }
+}
