9128d09d6e427e2f9fce0ce039bd90adc6dfa997
AndreyGanin <ganin25@inbox.ru>
2025-10-21 15:16:40 +0500

    Task4

---
+++ Tyuiu.GaninAS.Sprint2.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.GaninAS.Sprint2.Task4.V30.Lib;
+
+namespace Tyuiu.GaninAS.Sprint2.Task4.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            double wait = 343;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
