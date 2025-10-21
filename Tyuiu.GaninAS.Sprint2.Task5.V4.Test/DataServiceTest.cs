951517522ad863361186d639cff1ea4da9967f53
AndreyGanin <ganin25@inbox.ru>
2025-10-21 16:46:38 +0500

    Task5

---
+++ Tyuiu.GaninAS.Sprint2.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.GaninAS.Sprint2.Task5.V4.Lib;
+
+namespace Tyuiu.GaninAS.Sprint2.Task5.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestFindCardNameAndValue()
+        {
+            DataService ds = new DataService();
+
+            Assert.AreEqual("пики", ds.FindCardSuit(1));
+            Assert.AreEqual("трефы", ds.FindCardSuit(2));
+            Assert.AreEqual("бубны", ds.FindCardSuit(3));
+            Assert.AreEqual("червы", ds.FindCardSuit(4));
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindCardSuit(-1);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindCardSuit(5);
+            });
+        }
+    }
+}
---
+++ Tyuiu.GaninAS.Sprint2.Task5.V4/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.GaninAS.Sprint2.Task5.V4.Lib;
+namespace Tyuiu.GaninAS.Sprint2.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Ганин Андрей Сергеевич | ПКТБ-25-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Ганин Андрей Сергеевич | ПКТБ-25-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*Написать программу, которая использует оператор switch вычисляет требуемо*");
+            Console.WriteLine("*ое значение и возвращает результат.                                      *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер масти: ");
+            int numCard = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numCard < 1 ) || (numCard > 4 ))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Это масть: " + ds.FindCardSuit(numCard);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
